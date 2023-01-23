ca9ecfc2755bfc1a42d40a75ab6a936c749cafbc
SarasLenka10 <saraswatilenka10@gmail.com>
2023-01-23 15:20:17 +0530

    task 3

---
+++ starting with dom.js
@@ -0,0 +1,37 @@
+//EXAMINE THE DOCUMENT 
+
+// console.dir(document);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// document.title = 123;
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// document.all[10].textContent = "hello";
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+
+// GETELEMNTBYID //
+// console.log(document.getElementById('header-title'));
+// let headerTitle = document.getElementById('header-title');
+// let header = document.getElementById('main-header');
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = 'Goodbye';
+// headerTitle.innerHTML = '<h3>Hello</h3>';
+// header.style.borderBottom = 'solid 3px #000'
+
+// GET ELEMENTS BY CLASS NAME //
+
+// let edit = document.getElementsByClassName('title');
+// console.log(edit[0]);
+// edit[0].style.fontWeight = 'bold';
+// edit[0].style.color = 'green';
+
+
+
+
+
