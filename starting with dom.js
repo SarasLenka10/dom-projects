e9a5aa2092656e05ebe594fcadfc418fd4808c9f
SarasLenka10 <saraswatilenka10@gmail.com>
2023-01-23 18:57:54 +0530

    task6

--- starting with dom.js
+++ starting with dom.js
@@ -60,6 +60,52 @@
 //     li[i].style.backgroundColor = 'tomato';
 //  }
 
+// QUERY SELECTOR //
+// let header = document.querySelector('#main-header');
+// header.style.borderBottom = ' solid 4px #ccc';
+
+// var input = document.querySelector('input');
+// input.value = 'I love myself'
+
+// let  submit = document.querySelector('input[type="submit"]');
+// submit.value="SEND"
+// submit.style.backgroundColor ='blue'
+
+// let item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
+
+// let lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color = 'blue';
+
+// // let secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// // secondItem.style.backgroundColor = 'green';
+
+// let thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.visibility = 'hidden';
+
+// QUERYSELECTORALL //
+// let titles = document.querySelectorAll('.title');
+// console.log(titles);
+// titles[0].textContent = 'Hello'
+
+// let secondItem = document.querySelectorAll('.list-group-item:nth-child(2)');
+// secondItem.style.color = 'green'
+
+
+// let odd = document.querySelectorAll('li:nth-child(odd)');
+// let even = document.querySelectorAll('li:nth-child(even)');
+
+// for(let i=0; i<odd.length; i++){
+//     odd[i].style.backgroundColor = 'green';
+    // even[i].style.backgroundColor = '#ccc';
+// }
+
+
+
+
+
+
+
 
 
 
