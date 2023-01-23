6af2c52d7daa371807c2a1ba30ef698688626219
SarasLenka10 <saraswatilenka10@gmail.com>
2023-01-23 17:04:13 +0530

    task5

--- starting with dom.js
+++ starting with dom.js
@@ -38,6 +38,30 @@
 // for(let i=0; i<items.length; i++){
 //     items[i].style.fontWeight = 'bold';
 //  }
+// let items = document.getElementsByClassName('list-group-item');
+// console.log(items[1]);
+// items[1].style.fontWeight = 'bold';
+// items[1].textContent = 'Hello 2';
+
+// for(let i=0; i<items.length; i++){
+//     items[i].style.backgroundColor = '#f4f4f4';
+//  }
+
+// GET ELEMENTS BY TAGNAME //
+// let li = document.getElementsByTagName('li');
+// console.log(li[1]);
+// li[1].style.fontWeight = 'bold';
+// li[1].textContent = 'Hello 2';
+
+// for(let i=0; i<li.length; i++){
+//     li[i].style.backgroundColor = '#f4f4f4';
+//  }
+// for(let i=0; i<li.length; i++){
+//     li[i].style.backgroundColor = 'tomato';
+//  }
+
+
+
 
 
 
