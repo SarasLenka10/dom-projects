77aac22dd523e21477f196475c6fe7a54589d92d
SarasLenka10 <saraswatilenka10@gmail.com>
2023-01-23 16:27:49 +0530

    task4

--- starting with dom.js
+++ starting with dom.js
@@ -31,6 +31,18 @@
 // edit[0].style.fontWeight = 'bold';
 // edit[0].style.color = 'green';
 
+// let items = document.getElementsByClassName('list-group-item');
+// console.log(items[2]);
+// items[2].style.backgroundColor = 'green';
+
+// for(let i=0; i<items.length; i++){
+//     items[i].style.fontWeight = 'bold';
+//  }
+
+
+
+
+
 
 
 
