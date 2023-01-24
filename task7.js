ee47ca484acaf3c2aa05b7b85a2dd1a520401a8c
SarasLenka10 <saraswatilenka10@gmail.com>
2023-01-24 13:21:19 +0530

    task7

---
+++ task7.js
@@ -0,0 +1,100 @@
+// TRAVERSING THE DOM //
+let itemList = document.querySelector('#items');
+// parentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor ='#f4f4f4'
+// console.log(itemList.parentNode.parentNode);
+
+// parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor ='#f4f4f4'
+// console.log(itemList.parentElement.parentElement);
+
+// childNodes
+// console.log(itemList.childNodes);
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor ='yellow';
+
+// firstChild
+// console.log(itemList.firstChild);
+// firstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello 1';
+
+// lastChild
+// console.log(itemList.lastChild);
+// lastElementChild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Hello 1';
+
+// nextSibling //
+// console.log(itemList.nextSibling);
+// console.log(itemList.nextElementSibling);
+
+// previousSibling
+// console.log(itemList.previousSibling);
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = 'green';
+
+// createElement
+
+// // Create a div
+// let newDiv = document.createElement('div');
+
+// // Add class
+// newDiv.className = 'hello';
+
+// // Add Id
+// newDiv.id = 'hello1'
+
+// // Add attribute
+// newDiv.setAttribute('title','Hello Div');
+
+// // Create text node
+// let newDivText = document.createTextNode('Hello');
+
+// // Add to div
+// newDiv.appendChild(newDivText);
+// console.log(newDiv);
+
+// let container = document.querySelector('header .container');
+// let h1 = document.querySelector('header h1');
+
+// // newDiv.style.fontSize = '30px';
+// // container.insertBefore(newDiv,h1);
+// // console.log(newDiv);
+
+// let newDiv = document.createElement('div');
+
+// // Add class
+// newDiv.className = 'hello';
+
+// // Add Id
+// newDiv.id = 'hello1'
+
+// // Add attribute
+// newDiv.setAttribute('title','Hello Div');
+
+// // Create text node
+// let newDivText = document.createTextNode('Hello');
+
+// // Add to div
+// newDiv.appendChild(newDivText);
+// // console.log(newDiv);
+
+// let ul = document.querySelector('.list-group');
+// console.log(ul);
+// let list = document.querySelector('.list-group-item');
+// console.log(list);
+// newDiv.style.fontSize = '30px';
+// ul.insertBefore(newDiv, list);
+// console.log(newDiv);
+
+
+
+
+
+
+
+
